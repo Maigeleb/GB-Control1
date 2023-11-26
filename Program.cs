99d3da68305c2758c07ef5dde0af9d207157ed3d
Mikhail Dorenskiy <masdoren@mail.ru>
2023-11-26 12:00:01 +0300

    First Step

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам
+
+Console.WriteLine("Enter string array:");
+string[] inputArray = GetArray(Console.ReadLine());
+
+Console.WriteLine("Entered array:");
+for (int i = 0; i < inputArray.Length; i++)
+{
+    Console.WriteLine(inputArray[i]);
+}
+
+string[] GetArray(string input)
+{
+    string[] res = input.Split(' ');
+
+    return res;
+}
