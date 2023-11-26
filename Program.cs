bce7f860dcab8880041be1fcfd226b1eac4a5ff2
Mikhail Dorenskiy <masdoren@mail.ru>
2023-11-26 12:21:09 +0300

    Second Step

--- Program.cs
+++ Program.cs
@@ -6,12 +6,32 @@ string[] inputArray = GetArray(Console.ReadLine());
 Console.WriteLine("Entered array:");
 for (int i = 0; i < inputArray.Length; i++)
 {
-    Console.WriteLine(inputArray[i]);
+    Console.WriteLine(i + ": " + inputArray[i]);
+}
+Console.WriteLine();
+
+string[] outputArray = WorkWithArray(inputArray);
+
+Console.WriteLine("Output Array:");
+for (int i = 0; i < outputArray.Length; i++)
+{
+    Console.WriteLine(i + ": " + outputArray[i]);
 }
 
-string[] GetArray(string input)
-{
-    string[] res = input.Split(' ');
+string[] GetArray(string input) => input.Split(' ');
 
+string[] WorkWithArray(string[] input)
+{
+    string[] res = Array.Empty<string>();
+    int counter = 1;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length < 4)
+        {
+            Array.Resize(ref res, counter);
+            res[counter - 1] = inputArray[i];
+            counter++;
+        }
+    }
     return res;
 }
